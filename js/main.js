d975fa9bdce406eb285ce9860edd8e92a2854ace
Brent P <brent.pfefferle@gmail.com>
2018-07-29 23:35:35 -0700

    Responsive landing page card down to 280px. Full height navbar collapse for mobile.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ hamburger.addEventListener('click', function(){
 	// Change the class of nav ul.
 	if (nav_list.className === 'nav-ul'){
 		nav_list.className = 'nav-ul nav-mobile';
+		$('.landing-page-card').hide();
 	} else{
 		nav_list.className = 'nav-ul';
+		$('.landing-page-card').show();
 	}
 });
 
